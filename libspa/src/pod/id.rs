cdacace55db29b4a110d3651d1d485cfa84fdbe3
Matthieu Legrand <legmatt0@gmail.com>
2023-06-05 16:22:36 +0200

    Changing the enum to the right name

--- libspa/src/pod/id.rs
+++ libspa/src/pod/id.rs
@@ -8,5 +8,5 @@ enum_from_sys!(MediaType, r"SPA_MEDIA_TYPE_([a-z0-9]+)");
 
 enum_from_sys!(MediaSubtype, r"SPA_MEDIA_SUBTYPE_([a-z0-9]+)");
 
-enum_from_sys!(Format, r"SPA_VIDEO_FORMAT_([A-Za-z0-9]+_[A-Za-z0-9]+)");
+enum_from_sys!(VideoFormat, r"SPA_VIDEO_FORMAT_([A-Za-z0-9]+_[A-Za-z0-9]+)");
 
